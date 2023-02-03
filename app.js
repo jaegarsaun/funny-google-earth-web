1f02d8b4a0114b832b230f4f8a4eb1e0b3de17b9
Jaegar <jaegarls@icloud.com>
2023-02-03 14:31:19 -0330

    Added prompt to look around at the top of the screen

--- app.js
+++ app.js
@@ -38,12 +38,5 @@ function initMap() {
         map.setStreetView(panorama);
       }
  
-  
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  }
+
   
